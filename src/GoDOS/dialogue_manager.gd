e3584081bda7f6f8c8321d72d5e4a44a196a31fb
pilgrim <35772798+EnoughSensei@users.noreply.github.com>
2019-09-28 21:24:52 +0300

    GoDOS integration

---
+++ src/GoDOS/dialogue_manager.gd
@@ -0,0 +1,91 @@
+extends Node2D
+
+var dialogue : Dialogue
+
+
+func load_dialogue (file_path) -> void:
+	dialogue._reset()
+	dialogue.data = load(file_path).new().data
+	$DialogueBox.show()
+	dialogue.start()
+
+
+func set_var (key : String, value : bool) -> void:
+	dialogue.variables[key] = value
+
+
+func button_clicked (name : String, option : Dictionary) -> void:
+	dialogue.is_button_active = false
+	set_var(option["var"], true)
+
+
+func merge_lines (data : Dictionary) -> String:
+	var pos = "start"
+	var string = ""
+	
+	while pos != "end":
+		if data[pos].has("data"):
+			if string != "":
+				string = string + '\n' + data[pos]["data"]
+			else:
+				string = data[pos]["data"]
+		
+		assert(data[pos].has("next"))
+		pos = data[pos]["next"]
+	
+	return string
+
+
+func add_button (name : String, option : Dictionary) -> void:
+	var button = Button.new()
+	button.name = name
+	button.text = merge_lines(option["data"])
+	
+	get_node("DialogueBox/OptionBox").add_child(button)
+	print("Added " + name + " button.")
+	button.connect("button_down", self, "button_clicked", [name, option])
+
+
+func show_buttons (options : Dictionary) -> void:
+	for name in options.keys():
+		add_button(name, options[name])
+
+
+func hide_buttons () -> void:
+	for child in get_node("DialogueBox/OptionBox").get_children():
+		child.hide()
+
+
+func clear_buttons () -> void:
+	for child in get_node("DialogueBox/OptionBox").get_children():
+		get_node("DialogueBox/OptionBox").remove_child(child)
+
+
+func on_text_change (text) -> void:
+	get_node("DialogueBox/Text").text = text
+	get_node("DialogueBox/Text").visible_characters = 0
+
+
+func on_dialogue_finished () -> void:
+	$DialogueBox.hide()
+	dialogue._reset()
+
+
+func _ready ():
+	print("Dialogue Manager is online.")
+	dialogue = Dialogue.new()
+	add_child(dialogue)
+	
+	dialogue.connect("showed_buttons", self, "show_buttons")
+	dialogue.connect("hided_buttons", self, "hide_buttons")
+	dialogue.connect("cleared_buttons", self, "clear_buttons")
+	dialogue.connect("text_changed", self, "on_text_change")
+	dialogue.connect("dialogue_finished", self, "on_dialogue_finished")
+	
+	load_dialogue("res://dialogues/example_dialogue.gd")
+
+
+func _process(delta):
+	get_node("DialogueBox/Label").text = str(dialogue.state.s)
+	get_node("DialogueBox/Label2").text = str(dialogue.variables)
+	get_node("DialogueBox/Text").visible_characters += 1
--- src/seedbag/seedbag.gd
+++ src/seedbag/seedbag.gd
@@ -8,25 +8,29 @@ var seed_count : int = 0
 export var icon : Texture
 
 func set_seed (new_seed : Seed) -> void:
-   _seed = new_seed
+	_seed = new_seed
 
 
 func get_seed () -> Seed:
-   return _seed
+	return _seed
 
 
 func set_seed_count (new_seed_count : int) -> void:
-   seed_count = new_seed_count
+	seed_count = new_seed_count
 
 
 func get_seed_count () -> int:
-   return seed_count
+	return seed_count
 
 
 func _init (_seed : Seed, seed_count : int):
-   self._seed = _seed
-   self.seed_count = seed_count
-
+	self._seed = _seed
+	set_seed_count(seed_count)
+	_ready()
 
 func _ready ():
-   name = "SeedBag"
+	assert(icon)
+	var sprite = Sprite.new()
+	sprite.name = "Icon"
+	sprite.texture = icon
+	add_child(sprite)
