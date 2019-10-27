f49a6dba56281a2b9b6f415cd8710dca9de178db
pilgrim <35772798+EnoughSensei@users.noreply.github.com>
2019-10-27 20:25:17 +0300

    Added Capo

--- src/farm/farm.gd
+++ src/farm/farm.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
 var seedbag_dictionary = preload("res://src/seedbagdictionary/seedbagdictionary.gd").new().data
+var item_dictionary = preload("res://src/itemdictionary/itemdictionary.gd").new().data
 
 onready var player = get_node("World/Player")
 onready var toolbar = get_node("World/Player/HUD/InventoryContainer/Toolbar")
+onready var inventory = get_node("World/Player/HUD/InventoryContainer/Inventory")
+
 
 func _ready():
 #	$"Dialogue Manager".load_dialogue("res://dialogues/example_dialogue.gd")
@@ -12,6 +15,9 @@ func _ready():
 	
 	toolbar.add_item_to_slot(0, {"item" : seedbag1, "count" : 1})
 	toolbar.add_item_to_slot(1, {"item" : seedbag2, "count" : 1})
+	
+	inventory.add_item_to_slot(4, {"item" : seedbag1, "count" : 1})
+	inventory.add_item_to_slot(8, {"item" : seedbag2, "count" : 1})
 
 
 func _process(delta):
--- src/player/player.gd
+++ src/player/player.gd
@@ -1,6 +1,77 @@
 extends KinematicBody2D
 
-const MOVE_SPEED = 200
+export var MOVE_SPEED = 200
+export var idle_front : SpriteFrames
+export var idle_back : SpriteFrames
+export var walk_front : SpriteFrames
+export var walk_back : SpriteFrames
 
-func move (move_vec : Vector2, move_speed := MOVE_SPEED):
+var is_idle := true
+var facing_right := true
+var facing_front := true
+var direction = "right"
+
+
+func set_animation_frame (frames : SpriteFrames) -> void:
+	if get_node("Sprite").frames != frames:
+		get_node("Sprite").frames = frames
+
+
+func flip_sprite () -> void:
+	get_node("Sprite").flip_h = !get_node("Sprite").flip_h
+
+
+func update_direction() -> void:
+	if facing_right == true and direction == "left":
+		direction = "right"
+		flip_sprite()
+	
+	elif facing_right == false and direction == "right":
+		direction = "left"
+		flip_sprite()
+
+
+func update_animation () -> void:
+	update_direction()
+	
+	if is_idle:
+		if facing_front:
+			set_animation_frame(idle_front)
+		
+		else:
+			set_animation_frame(idle_back)
+	
+	else:
+		if facing_front:
+			set_animation_frame(walk_front)
+		
+		else:
+			set_animation_frame(walk_back)
+
+
+func move (move_vec : Vector2, move_speed := MOVE_SPEED) -> void:
 	move_and_slide(move_vec.normalized() * move_speed)
+	is_idle = false
+	get_node("IdleTimer").start()
+	
+	if move_vec.x != 0:
+		facing_right = move_vec.x > 0
+	
+	if move_vec.y != 0:
+		facing_front = move_vec.y > 0
+	
+	update_animation()
+
+
+func _on_IdleTimer_timeout():
+	is_idle = true
+	update_animation()
+
+
+var action_key_array = [
+	["move_up", funcref(self, "move_up")],
+	["move_down", funcref(self, "move_down")],
+	["move_left", funcref(self, "move_left")],
+	["move_right", funcref(self, "move_right")],
+	["use_item", funcref(self, "use_item")]
+]
